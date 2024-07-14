d2b8fa90767b3d56a2440ff5d86f985a3e528c7b
cyber9ja <ayodejioladapo15@gmail.com>
2024-07-14 22:49:32 +0100

    minor fix

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,10 +1,17 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 import * as bcrypt from 'bcryptjs'
 import * as jwt from 'jsonwebtoken'
 import { UserType } from '@prisma/client';
 
 interface signUpParams {
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  userType: UserType
+}
+interface authParams {
   name: string;
   email: string;
   password: string;
@@ -21,13 +28,14 @@ interface signInParams {
 export class AuthService {
   constructor(private readonly databaseService: DatabaseService) {}
 
-  async signUp({ name, email, password, phone, userType }: signUpParams) {
+  // async signUp({ name, email, password, phone, userType }: signUpParams) {
+  async signUp({ name, email, password, phone, userType }: Partial<authParams>) {
     const userExists = await this.databaseService.user.findUnique({
       where: { email }
     });
 
     if(userExists) {
-      throw new ConflictException('Username already exist')
+      throw new ConflictException('User already exists')
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -40,8 +48,18 @@ export class AuthService {
         password: hashedPassword,
         userType
       },
+      select: {
+        userId: true,
+        email: true,
+        name: true,
+        phone: true,
+        userType: true
+      }
     });
 
+    if(!user){
+      return new BadRequestException('An error has occurred')
+    }
     return user
   }
 }
