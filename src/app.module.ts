969a50035e09bdc53b099519a36c4b03e180f13c
cyber9ja <ayodejioladapo15@gmail.com>
2024-07-16 17:08:47 +0100

    created product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { DatabaseModule } from './database/database.module';
 import { AuthController } from './user/auth/auth.controller';
 import { AuthService } from './user/auth/auth.service';
 import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, UserModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, UserModule, ProductModule],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
