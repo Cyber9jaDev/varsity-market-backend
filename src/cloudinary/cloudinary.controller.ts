9d0eb5a85f45d0b066143e4619f53ce6dbb70372
cyber9ja <ayodejioladapo15@gmail.com>
2024-08-03 22:07:09 +0100

    uploadImage to handle multiple files

--- src/cloudinary/cloudinary.controller.ts
+++ src/cloudinary/cloudinary.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CloudinaryService } from './cloudinary.service';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('image')
 export class CloudinaryController {
   constructor(private readonly cloudinaryService: CloudinaryService) {}
 
   @Post('/upload')
-  @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file: Express.Multer.File){
-    console.log(file);
-    return await this.cloudinaryService.uploadImage(file, 'unimarket/posts');
+  @UseInterceptors(FilesInterceptor('files'))
+  async uploadFile(@UploadedFiles() files: Express.Multer.File[]) {
+    const uploadPromises = files.map((file) =>
+      this.cloudinaryService.uploadImage(file, 'unimarket/posts'),
+    );
+    const results = await Promise.all(uploadPromises);
+    return results;
   }
 }
