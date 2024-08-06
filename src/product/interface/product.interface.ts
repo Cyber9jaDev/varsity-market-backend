404c60e7a144b9821932fdc5961dfba112b2baf6
cyber9ja <ayodejioladapo15@gmail.com>
2024-08-06 15:04:27 +0100

    updated filter

--- src/product/interface/product.interface.ts
+++ src/product/interface/product.interface.ts
@@ -1,25 +1,39 @@
-import { CategoryType, ConditionType, Location } from "@prisma/client";
+import { CategoryType, ConditionType, Location } from '@prisma/client';
 
-export interface createProductParams{
+export interface FilterQueries {
+  searchText?: string,
+  category?: CategoryType,
+  location?: Location,
+  dateFrom?: Date,
+  dateTo?: Date,
+  sortBy?: string,
+  page?: number,
+  price?: { 
+    gte?: number, 
+    lte?: number,
+  }
+}
+
+export interface createProductParams {
   name: string;
   description: string;
   price: number;
   category: CategoryType;
-  condition: ConditionType,
-  location: Location,
+  condition: ConditionType;
+  location: Location;
 }
 
-export interface ProductImageParams{
-  secure_url: string,  
-  asset_id: string, 
-  public_id: string 
+export interface ProductImageParams {
+  secure_url: string;
+  asset_id: string;
+  public_id: string;
 }
 
-export interface UpdateProductInterface{
+export interface UpdateProductInterface {
   name?: string;
   description?: string;
   price?: number;
   category?: CategoryType;
-  condition?: ConditionType,
-  location?: Location,
+  condition?: ConditionType;
+  location?: Location;
 }
