d9050125effee1029a7451b29e72a0e3e1973415
cyber9ja <ayodejioladapo15@gmail.com>
2024-08-09 21:38:18 +0100

    updated chat controller

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ChatService } from './chat.service';
+import { CreateChatDto } from './dtos/chat.dto';
 
 @Controller('chat')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
   @Post('/initiate-chat')
-  initiateChat(@Body() chat: any) {
-    return this.chatService.initiateChat();
+  initiateChat(@Body() { senderId, receiverId }: CreateChatDto) {
+    return this.chatService.initiateChat(senderId, receiverId);
   }
+
+  
 }
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -5,9 +5,34 @@ import { DatabaseService } from 'src/database/database.service';
 export class ChatService {
   constructor(private readonly databaseService: DatabaseService) {}
 
-  async initiateChat(){
-    // const createChat = await this.databaseService.chat.create({
-    //   data: 
-    // })
+  async initiateChat(senderId: string, receiverId: string) {
+    const previousChat = await this.databaseService.chat.findFirst({
+      where: {
+        participants: {
+          every: {
+            participantId: {
+              in: [senderId, receiverId],
+            },
+          },
+        },
+      },
+    });
+
+    if(!previousChat){
+      const newChat = await this.databaseService.chat.create({
+        data: {
+          participants: {
+            create: [
+              {participantId: senderId},
+              {participantId: receiverId},
+            ]
+          }
+        }
+      });
+      
+      return { message: 'Chat not found' };
+    }
+
+    return { message: "Chat created"}
   }
 }
