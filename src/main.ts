ea7899a56b078da456916b0827d51c379d7bc300
cyber9ja <ayodejioladapo15@gmail.com>
2024-12-10 14:27:58 +0000

    minor fix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const PORT = process.env.PORT || 3001
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log']
@@ -24,6 +26,6 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  await app.listen(3001);
+  await app.listen(PORT);
 }
 bootstrap();
